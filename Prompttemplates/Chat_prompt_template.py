a3cb8273fbd8e48ceb3740e8686cd6af0fc4dc91
bhuwanthapa45 <hellobhuwanthapa45@gmail.com>
2025-10-12 22:00:55 +0530

    proxy commit

--- Prompttemplates/Chat_prompt_template.py
+++ Prompttemplates/Chat_prompt_template.py
@@ -5,6 +5,6 @@ chat_template = ChatPromptTemplate([
     ('human', 'Explain in simple terms, what is {topic}')
 ])
 
-prompt = chat_template.invoke({'domain':'cricket','topic':'Dusra'})
+promptt = chat_template.invoke({'domain':'cricket','topic':'Dusra'})
 
-print(prompt)
+print(promptt)
