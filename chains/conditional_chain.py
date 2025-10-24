fdb9888c17ff1dc63563bc008c963593ac42f96f
bhuwanthapa45 <hellobhuwanthapa45@gmail.com>
2025-10-24 22:03:46 +0530

    just checking

--- chains/conditional_chain.py
+++ chains/conditional_chain.py
@@ -46,6 +46,6 @@ branch_chain = RunnableBranch(
 
 chain = classifier_chain | branch_chain
 
-print(chain.invoke({'feedback': 'Thiss is a beautiful phone'}))
+print(chain.invoke({'feedback': 'This is a beautiful phone'}))
 
 chain.get_graph().print_ascii()
